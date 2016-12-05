8a53dba3f2a5f6a78c14b114aad43c6aa2b7790e
Sawood Alam <ibnesayeed@gmail.com>
2016-12-05 16:33:07 -0500

    Added -q flag to supress errors

--- log_processors/iawb_log_url_extractor.py
+++ log_processors/iawb_log_url_extractor.py
@@ -6,15 +6,22 @@
 #   Ignores the lines that do not look like a URL.
 #   For better canonicalization, it returns SURT form instead.
 # Requirements: The system needs python and "surt" package installed.
+# Usage:
+#     STDIN | ./iawb_log_url_extractor.py | STDOUT
+#     STDIN | ./iawb_log_url_extractor.py -q | STDOUT
+#   Errors are written to the STDERR that can be supressed using -q flag
 # Author: Sawood Alam
 
 import sys
 import surt
 import re
 
-pattern = re.compile("http://web\.archive\.org/web/(\*|\d{14})/(.*)")
-
 if __name__ == "__main__":
+    pattern = re.compile("http://web\.archive\.org/web/(\*|\d{14})/(.*)")
+    # Supress error if -q flag is set
+    if "-q" in sys.argv:
+        sys.stderr = open('/dev/null', 'w')
+
     for line in sys.stdin:
         m = re.match(pattern, line)
         suri = "-"
